ab7a35375ec8c94170fdd51db66fdc738a2953ed
furleyman-hub <furleyman@gmail.com>
2025-12-04 01:26:59 -0500

    Update __init__.py

--- generator/__init__.py
+++ generator/__init__.py
@@ -1,50 +1,51 @@
-"""Utilities for generating training class materials."""
-
-from .models import (
-    ClassOutline,
-    GeneratedPackage,
-    InstructorGuide,
-    InstructorSection,
-    OutlineSection,
-    QuickRefStep,
-    QuickReferenceGuide,
-    VideoScript,
-    VideoSegment,
-)
-from .audio_processing import synthesize_narration_audio, transcribe_audio_files
-from .llm_openai import (
+from .generator import (
     generate_class_outline,
     generate_instructor_guide,
     generate_quick_reference,
     generate_video_script,
 )
-from .markdown_export import (
-    instructor_guide_to_markdown,
-    outline_to_markdown,
-    quick_ref_to_markdown,
-    video_script_to_markdown,
+
+from .models import (
+    ClassOutline,
+    OutlineSection,
+    InstructorGuide,
+    InstructorSection,
+    VideoScript,
+    VideoSegment,
+    QuickReferenceGuide,
+    QuickRefStep,
 )
-from .text_extraction import extract_text_from_files
+
+from .markdown_utils import (
+    outline_to_markdown,
+    instructor_guide_to_markdown,
+    video_script_to_markdown,
+    quick_ref_to_markdown,
+)
+
+from .extract_text import extract_text_from_files
+from .audio import transcribe_audio_files, synthesize_narration_audio
+
 
 __all__ = [
-    "ClassOutline",
-    "GeneratedPackage",
-    "InstructorGuide",
-    "InstructorSection",
-    "OutlineSection",
-    "QuickRefStep",
-    "QuickReferenceGuide",
-    "VideoScript",
-    "VideoSegment",
-    "synthesize_narration_audio",
-    "transcribe_audio_files",
     "generate_class_outline",
     "generate_instructor_guide",
     "generate_quick_reference",
     "generate_video_script",
-    "instructor_guide_to_markdown",
     "outline_to_markdown",
-    "quick_ref_to_markdown",
+    "instructor_guide_to_markdown",
     "video_script_to_markdown",
+    "quick_ref_to_markdown",
     "extract_text_from_files",
+    "transcribe_audio_files",
+    "synthesize_narration_audio",
+    # models
+    "ClassOutline",
+    "OutlineSection",
+    "InstructorGuide",
+    "InstructorSection",
+    "VideoScript",
+    "VideoSegment",
+    "QuickReferenceGuide",
+    "QuickRefStep",
 ]
